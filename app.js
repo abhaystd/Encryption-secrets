71199c1593ed27dd56484e91b7409cfb9b2980ce
ab ya <abstyle0701@gmail.com>
2024-08-04 12:55:50 +0530

    level 2 -Encryption

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const bodyParser=require ("body-parser");
 const ejs= require("ejs");
 const mongoose =require("mongoose");
-// const encrypt =require('mongoose-encryption')
+const encrypt =require('mongoose-encryption')
 
 const app= express();
 
@@ -17,8 +17,8 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
-// const secret ="thisisourlittlesecret.";
-// userSchema.plugin(encrypt,{secret:secret ,encryptedFields: ['password']});
+const secret ="thisisourlittlesecret.";
+userSchema.plugin(encrypt,{secret:secret ,encryptedFields: ['password']});
 
 const User= new mongoose.model('User',userSchema);
 
