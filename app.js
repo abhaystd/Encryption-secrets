13d36f97422ef6ceeaf72b215ee3325e865dbb70
ab ya <abstyle0701@gmail.com>
2024-08-04 18:39:16 +0530

    level 3 Hashing password

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ const express=require("express");
 const bodyParser=require ("body-parser");
 const ejs= require("ejs");
 const mongoose =require("mongoose");
-const encrypt =require('mongoose-encryption');
+// const md5 =require("md5");  less secure Hash
+const sha512 =require("js-sha512");  //more secure Hash
 
 const app= express();
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
-
-console.log(process.env.API_KEY);
-
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -21,10 +19,7 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET ,encryptedFields: ['password']});
-
 const User= new mongoose.model('User',userSchema);
-
 
 
 app.get("/",async function(req,res){
@@ -42,7 +37,8 @@ app.post("/register",async function(req,res){
     try{
         const newUser=new User({
             email:req.body.username,
-            password:req.body.password
+            // password:md5(req.body.password)
+            password:sha512(req.body.password)
         });
         await newUser.save();
         res.render("secrets");
@@ -54,7 +50,8 @@ app.post("/register",async function(req,res){
 
 app.post("/login",async function (req,res) {
     const loginId=req.body.username;
-    const loginPass=req.body.password;
+    // const loginPass=md5(req.body.password);
+    const loginPass=sha512(req.body.password);
     await User.findOne({email:loginId})
     .then(function(foundUser){
         if(foundUser.password===loginPass){
